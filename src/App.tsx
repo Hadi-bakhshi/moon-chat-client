be2e674a2f706ecddd4589003e50dba4dba090de
hadi-bakhshi <hadibakhshi277@gmail.com>
2022-09-30 13:02:51 +0330

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen m-auto bg-gray-100">
+      <h2 className="bg-indigo-300">Welcome to moon application</h2>
+    </div>
+  );
+}
+
+export default App;
